6ed21e182ac05f1e1487fb7e46790d4b46e23842
Watcharawut Pornsawat <s6101012630160@email.kmutnb.ac.th>
2020-02-24 15:22:33 +0700

    now can use operate + - * / and show result in templates

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    if request.POST.get("find value"):
+        num = request.POST['x']
+        num2 = request.POST['y']
+        operate = request.POST["operate_list"]
+        if operate == "+":
+            result = int(num) + int(num2)
+
+            return render(request,"calculator/home.html",{"result":result,"operate":operate})
+        elif operate == "-":
+            result = int(num) - int(num2)
+
+            return render(request, "calculator/home.html", {"result": result, "operate": operate})
+        elif operate == "*":
+            result = int(num) * int(num2)
+            return render(request,"calculator/home.html",{"result":result,"operate":operate})
+        elif operate == "/":
+            result = int(num) / int(num2)
+            return render(request,"calculator/home.html",{"result":result,"operate":operate})
+        return render(request, "calculator/home.html", {"operate": operate,"x":num,"y":num2})
+
+    return render(request,"calculator/home.html")
