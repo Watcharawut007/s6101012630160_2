374225eca0de4d65139f766d37eb4b08ba8a970f
Watcharawut Pornsawat <s6101012630160@email.kmutnb.ac.th>
2020-02-27 14:54:08 +0700

    add link go to post method and get method

--- calculator_get/views.py
+++ calculator_get/views.py
@@ -19,10 +19,10 @@ def home(request):
     string_list = []
     for history in history_list:
         string_list.append(str(history.num1) + history.operate + str(history.num2) + " " +"is"+" "+":"+str(history.result))
-    if request.POST.get("find value"):
-        num = request.POST['x']
-        num2 = request.POST['y']
-        operates = request.POST.getlist("operate_list")
+    if request.GET.get("find value"):
+        num = request.GET['x']
+        num2 = request.GET['y']
+        operates = request.GET.getlist("operate_list")
         if num == '' or num2 == '' or operates == []:
             return render(request,"get_templates/home.html",{"result_list":"plz enter your input"})
         else :
@@ -31,10 +31,10 @@ def home(request):
                 object = Calculate_get.objects.create()
                 result = calculating(num,num2,operate)
                 result_list[operate]=result
-                object.caculate(num, num2, operate, result)
+                object.calculate(num, num2, operate, result)
 
         return render(request, "get_templates/home.html", {"operate": operate,'op':operates, "x": num, "y": num2,"result_list":result_list,"history":string_list})
-    if request.POST.get('clear_history'):
+    if request.GET.get('clear_history'):
         for history in history_list:
             history.delete()
         return render(request,"get_templates/home.html",{"history":Calculate_get.objects.all()})
