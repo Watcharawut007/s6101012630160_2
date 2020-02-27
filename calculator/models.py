6c1dd1f76d14c7cc1cbb2d624f4b0a8721db4f6e
Watcharawut Pornsawat <s6101012630160@email.kmutnb.ac.th>
2020-02-27 14:13:26 +0700

    refactor calculate function

--- calculator/models.py
+++ calculator/models.py
@@ -3,8 +3,14 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-class Subject(models.Model):
+class Calculate(models.Model):
     num1 = models.IntegerField(default=0)
     num2 = models.IntegerField(default=0)
     operate = models.TextField(blank=True)
     result = models.IntegerField(default=0)
+    def calculate(self,int1,int2,operate2,result2):
+        self.num1 = int1
+        self.num2 = int2
+        self.operate = operate2
+        self.result =result2
+        self.save()
--- calculator/views.py
+++ calculator/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
+from calculator.models import Calculate
+
+
+def calculating(num, num2, operate):
+    if operate == "+":
+        resultvalue = int(num) + int(num2)
+    elif operate == "-":
+        resultvalue = int(num) - int(num2)
+    elif operate == "*":
+        resultvalue = int(num) * int(num2)
+    elif operate == "/":
+        resultvalue = int(num) / int(num2)
+    return resultvalue
 def home(request):
-    if request.POST.get("find value"):
-        num = request.POST['x']
-        num2 = request.POST['y']
-        operate = request.POST.getlist("operate_list")
-        result_list = {}
-        for operate in operate:
+    history_list = Calculate.objects.all()
+    string_list = []
+    for history in history_list:
+        string_list.append(str(history.num1) + history.operate + str(history.num2) + " " +"is"+" "+":"+str(history.result))
+    if request.GET.get("find value"):
+        num = request.GET['x']
+        num2 = request.GET['y']
+        operates = request.GET.getlist("operate_list")
+        if num == '' or num2 == '' or operates == []:
+            return render(request,"calculator/home.html",{"result_list":"plz enter your input"})
+        else :
+            result_list = {}
+            for operate in operates:
+                object = Calculate.objects.create()
+                result = calculating(num,num2,operate)
+                result_list[operate]=result
+                object.calculate(num, num2, operate, result)
 
-            if operate == "+":
-                result = int(num) + int(num2)
-                result_list[operate]=result
-            elif operate == "-":
-                result = int(num) - int(num2)
-                result_list[operate]=result
-            elif operate == "*":
-                result = int(num) * int(num2)
-                result_list[operate]=result
-            elif operate == "/":
-                result = int(num) / int(num2)
-                result_list[operate]=result
-        return render(request, "calculator/home.html", {"result": result, "operate": operate, "x": num, "y": num2,"result_list":result_list})
+        return render(request, "calculator/home.html", {"operate": operate,'op':operates, "x": num, "y": num2,"result_list":result_list,"history":string_list})
+    if request.GET.get('clear_history'):
+        for history in history_list:
+            history.delete()
+        return render(request,"calculator/home.html",{"history":Calculate.objects.all()})
+    return render(request,"calculator/home.html",{"history":string_list})
 
-    return render(request,"calculator/home.html")
+
